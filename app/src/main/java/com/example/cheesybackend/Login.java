7ad586077f366a1e495949f6bed495e01e2f15a8
Christopher <obanca@farmingdale.edu>
2021-10-31 18:06:18 -0400

    Made splash screen timed and it moves on automatically to login. After entering email in login it will either move to register or continue with the login process. Also removed Register.xml since it wasn't being used.

--- app/src/main/java/com/example/cheesybackend/Login.java
+++ app/src/main/java/com/example/cheesybackend/Login.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,11 +20,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
 
     private EditText editTextemail, editTextpassword;
+    private TextView pass;
     Button login;
 
     private FirebaseAuth mAuth;
@@ -38,6 +41,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
 
         editTextemail = findViewById(R.id.email);
         editTextpassword = findViewById(R.id.password);
+        pass = findViewById(R.id.viewpassword);
         mAuth = FirebaseAuth.getInstance();
     }
 
@@ -55,36 +59,54 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
 
         String email = editTextemail.getText().toString().trim();
         String password = editTextpassword.getText().toString().trim();
-
-        if(email.isEmpty()){
-            editTextemail.setError("Email is required");
-            editTextemail.requestFocus();
-            return;
-        }
-        if(password.isEmpty()){
-            editTextpassword.setError("password is required");
-            editTextpassword.requestFocus();
-        }
-        String message = "";
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-
-
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(Login.this, MainActivity2.class));
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(message,"sign in fail", task.getException());
-                            Toast.makeText(Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+        if(editTextpassword.getVisibility()==View.GONE){
+            mAuth.fetchSignInMethodsForEmail(email)
+                    .addOnCompleteListener(this,new OnCompleteListener<SignInMethodQueryResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                            boolean check = !task.getResult().getSignInMethods().isEmpty();
+                            if(!check)
+                                //if email can't be found in database user is sent to register activity
+                                startActivity(new Intent(Login.this,Register.class));
+                            else {
+                                //password field appear
+                                editTextpassword.setVisibility(View.VISIBLE);
+                                pass.setVisibility(View.VISIBLE);
+                            }
                         }
-                    }
-                });
+                    });
+        }
+        else {
 
+            if (email.isEmpty()) {
+                editTextemail.setError("Email is required");
+                editTextemail.requestFocus();
+                return;
+            }
+            if (password.isEmpty()) {
+                editTextpassword.setError("password is required");
+                editTextpassword.requestFocus();
+            }
+            String message = "";
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Login.this, MainActivity2.class));
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(message, "sign in fail", task.getException());
+                                Toast.makeText(Login.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
